db0d15991a6df0398df3ebabd11dbad606f658e4
Trevor Adcock <trevor.adcock@mac.com>
2018-11-01 14:29:15 -0600

    Initial Commit

---
+++ NewStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewStream
+//
+//  Created by DevMountain on 11/1/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewStream
+//
+//  Created by DevMountain on 11/1/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
